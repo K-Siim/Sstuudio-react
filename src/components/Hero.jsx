6d46aff521a32e93efafe05167f423a26429981a
K-Siim <kevinsiim007@gmail.com>
2025-04-14 20:28:44 +0300

    Hero section fix on the smaller devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import ProductImage from "../assets/Images/1000006482.jpg";
 
-
-
 const Hero = () => {
     return (
       <div
-  className="relative w-full h-screen flex items-center justify-start bg-cover bg-center px-10"
-  style={{
-    backgroundImage: `url(${ProductImage})`,
-  }}
->
-  
-  <div className="absolute inset-0 bg-black opacity-20 md:opacity-0"></div>
-
-  
-  <div className="relative z-10 ml-auto text-center mt-[-20rem] ">
-    <h1 className="text-5xl text-white md:text-black">Keraamika ja käsitöö</h1>
-    <p className="text-2xl text-white mt-2 text-center md:text-black">Teeme sinu unistused teoks!</p>
-    <p className="text-xl font-bold text-white text-center mt-2 md:text-black">S-Stuudio OÜ</p>
-  </div>
-</div>
+        className="relative w-full h-[85vh] flex items-center justify-start bg-cover bg-center px-10"
+        style={{
+          backgroundImage: `url(${ProductImage})`,
+          backgroundPosition: 'center top'
+        }}
+      >
+        
+        <div className="absolute inset-0 bg-black opacity-20 md:opacity-0"></div>
+        
+        <div className="relative z-10 ml-auto text-center md:mt-[-20rem] mt-[0]">
+          <h1 className="text-4xl md:text-5xl text-white md:text-black">Keraamika ja käsitöö</h1>
+          <p className="text-xl md:text-2xl text-white mt-2 text-center md:text-black">Teeme sinu unistused teoks!</p>
+          <p className="text-lg md:text-xl font-bold text-white text-center mt-2 md:text-black">S-Stuudio OÜ</p>
+        </div>
+      </div>
     );
   };
   
